e364f3b1f49e626cb9542db3e4023492e82f32e4
CYPARK <chanyoung990704@naver.com>
2023-10-31 22:23:41 +0900

    짜투리 커밋 나중에 로그 병합해도 됨

--- capstone/src/main/java/unit/capstone/domain/MovieComment.java
+++ capstone/src/main/java/unit/capstone/domain/MovieComment.java
@@ -13,6 +13,10 @@ public class MovieComment {
     private Long id;
     private String comment;
     private LocalDateTime createDate;
+    //rating 점수 1점에서 5점 정수형으로
+    //ratiing을 입력하지 않았으면 기본값 -1
+    @Column(columnDefinition = "int default -1")
+    private int rating;
 
     // 반대쪽 위치에서 @OnetoMany를 사용할 것에 대한 당위성 체크
     @ManyToOne(fetch = FetchType.LAZY)
@@ -29,9 +33,19 @@ public class MovieComment {
     }
 
 
+    //rating이 입력되지 않은 경우의 생성자
     public MovieComment(String comment, LocalDateTime createDate, Member member, Movie movie) {
         this.comment = comment;
         this.createDate = createDate;
+        this.member = member;
+        this.movie = movie;
+    }
+
+    //rating이 입력된 경우의 생성자
+    public MovieComment(String comment, LocalDateTime createDate, int rating, Member member, Movie movie) {
+        this.comment = comment;
+        this.createDate = createDate;
+        this.rating = rating;
         this.member = member;
         this.movie = movie;
     }
